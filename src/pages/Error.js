8241b86e9b759ad48765bc7e15342fb86bb2a816
amb1tionz <amb1tionz@hotmail.com>
2019-12-04 00:47:37 +0100

    today's work

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
 
 export default function Error() {
-    return <Hero />
+    return <Hero hero='defaultHero'>
+        <Banner title='404' subtitle='Page Not Found!'>
+            <Link to='/' className='btn-primary'>
+                Return home
+            </Link>
+        </Banner>
+    </Hero>
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import Hero from "../components/Hero"
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
+import Services from '../components/Services'
 
-export const Home = () => {
-    return <Hero hero="defaultHero"></ Hero>
+export default function Home () {
+    return (
+        <>
+            <Hero hero="defaultHero">
+                <Banner title="Luxurious Rooms" subtitle="Deluxe rooms starting at $299"> 
+                    <Link to='/rooms' className='btn-primary'>
+                        Our Rooms
+                    </Link>
+                </Banner>
+            </ Hero>
+            <Services />
+        </>
+    )
 }
-
-export default Home
