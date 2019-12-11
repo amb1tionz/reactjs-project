586390b7a837a1f3e305243d168d3b70384495f5
amb1tionz <vladtraykov@gmail.com>
2019-12-11 18:04:19 +0100

    all filters completed

--- src/context.js
+++ src/context.js
@@ -58,7 +58,7 @@ class RoomProvider extends Component {
 
   handleChange = event => {
     const target = event.target
-    const value = event.type === 'check' ? target.checked : target.value
+    const value = target.type === 'checkbox' ? target.checked : target.value
     const name = event.target.name
 
     this.setState({
@@ -83,6 +83,7 @@ class RoomProvider extends Component {
 
     // transform values
     capacity = parseInt(capacity)
+    price = parseInt(price)
 
 
     // filter by type
@@ -94,6 +95,24 @@ class RoomProvider extends Component {
     if (capacity !== 1) {
       tempRooms = tempRooms.filter(room => room.capacity >= capacity)
     }
+    
+    // filter by price
+    tempRooms = tempRooms.filter(room => room.price <= price)
+
+    // filter by size
+    tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize)
+    
+    // filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter(room => room.breakfast === true)
+    }
+
+    // filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter(room => room.pets === true)
+    }
+
+    // change state
     this.setState({
       sortedRooms: tempRooms
     })
